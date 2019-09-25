d38ac5371333f816d8341dc5c1f6e6438328203b
abraham <abraham@corehubdev.corehub>
2019-09-25 08:14:21 +0530

    update

---
+++ go/src/github.com/go-abraham-corehub/photobook/photobook.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/hex"
+	"log"
+	"net/http"
+	"strings"
+	"text/template"
+
+	"crypto/sha1"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+//Page is a type to implement HTML Page
+type Page struct {
+	Title string
+	Body  []byte
+	User  string
+}
+
+const dataDir = "data"
+const tmplDir = "tmpl/loginV8"
+
+var db *sql.DB
+
+var templates = template.Must(template.ParseFiles(tmplDir+"/"+"login.html", tmplDir+"/"+"home.html"))
+
+func main() {
+	startWebApp()
+}
+
+func startWebApp() {
+	http.Handle("/static/", //final url can be anything
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir(tmplDir+"/"+"static"))))
+	http.HandleFunc("/", handlerLogin)
+	http.HandleFunc("/authenticate", handlerAuthenticate)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func dbCheckCredentials(username string, password string) (int, string, bool) {
+	db, err := sql.Open("sqlite3", "./db/pb.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	username, fl := conditionString(username)
+	if !fl {
+		return 0, "", false
+	}
+
+	username = "\"" + username + "\""
+
+	password, fl = conditionString(password)
+	if !fl {
+		return 0, "", false
+	}
+	password = "\"" + password + "\""
+
+	queryString := "select role, name from user where username == " + username + " and password == " + password
+	rows, err := db.Query(queryString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+		var role int
+		var name string
+		err = rows.Scan(&role, &name)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			return role, name, true
+		}
+	}
+	return 0, "", false
+}
+
+func conditionString(str string) (string, bool) {
+	flag := true
+	strN := str
+	charsTrim := []byte{
+		' ',
+		'\\',
+		'"',
+	}
+	for _, cH := range charsTrim {
+		strN = strings.ReplaceAll(strN, string(cH), "")
+	}
+	if len(str) != len(strN) {
+		flag = false
+	}
+	return str, flag
+}
+
+func handlerLogin(w http.ResponseWriter, r *http.Request) {
+	title := "login"
+	p, err := loadPage(title, "", -1)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	renderTemplate(w, title, p)
+}
+
+func handlerAuthenticate(w http.ResponseWriter, r *http.Request) {
+
+	uN := r.FormValue("username")
+	pW := r.FormValue("password")
+
+	pWH := sha1.New()
+	pWH.Write([]byte(pW))
+
+	pWHS := hex.EncodeToString(pWH.Sum(nil))
+
+	title := "login"
+	role, user, isValid := dbCheckCredentials(uN, pWHS)
+	if isValid {
+		title = "home"
+	}
+
+	p, err := loadPage(title, user, role)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	renderTemplate(w, title, p)
+
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func loadPage(title string, user string, role int) (*Page, error) {
+	p := &Page{Title: title}
+	switch title {
+	case "home":
+		switch role {
+		case -7:
+			p.User = "Administrator"
+			title = "home-admin"
+		default:
+			p.User = user
+			title = "home-user"
+		}
+	}
+	/*
+		filename := dataDir + "/" + title + ".txt"
+		body, err := ioutil.ReadFile(filename)
+		if err != nil {
+			return nil, err
+		}
+
+		p.Body = body
+	*/
+	return p, nil
+}
---
+++ go/src/github.com/go-abraham-corehub/scratch-pad/scratch-pad.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/sha1"
+	"database/sql"
+	"encoding/hex"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	testDb()
+	hashTest("admin")
+	hashTest("abraham")
+}
+func hashTest(str string) []byte {
+	pW := str
+	pWH := sha1.New()
+	pWH.Write([]byte(pW))
+
+	pWHS := hex.EncodeToString(pWH.Sum(nil))
+
+	fmt.Println(pW, pWHS)
+	return []byte(pWHS)
+}
+
+type parseState struct {
+	inTag, isPre, isScript, isStyle, isSpace, isSkip bool
+}
+
+func setParseState(b byte, pS *parseState) {
+	pS.inTag = b>>5&1 == 1
+	pS.isPre = b>>4&1 == 1
+	pS.isScript = b>>3&1 == 1
+	pS.isStyle = b>>2&1 == 1
+	pS.isSpace = b>>1&1 == 1
+	pS.isSkip = b&1 == 1
+}
+
+func testDb() {
+	db, err := sql.Open("sqlite3", "../photobook/db/pb.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select username, password from user")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var un string
+		var pw string
+		err = rows.Scan(&un, &pw)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(un, pw)
+	}
+}
